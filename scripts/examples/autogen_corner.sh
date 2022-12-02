d02c8f26a23985e8d3b9a0b6dbdf8e2b6c8a3fb7
hyoshida <ironoriyh@gmail.com>
2022-12-02 10:40:55 +0900

    script

--- scripts/examples/autogen_corner.sh
+++ scripts/examples/autogen_corner.sh
@@ -1,20 +1,34 @@
-
+#!/bin/bash
 INPUT="./03_pointmesh/corner5.ply"
 
-python scripts/process_data/poisson_disk.py $INPUT
 
-INPUT="${INPUT%.*}.xyz"
+
+INPUT2="${INPUT%.*}.xyz"
 HULL="${INPUT%.*}_hull.obj"
-SAVEDIR="${INPUT%.*}/" #${INPUT%/*}
+SAVEDIR="${INPUT%.*}_upsample/" #${INPUT%/*}
 
-# if [ ! -f $INPUT ]
-# then
-    # echo "$INPUT not found" && \
-    python scripts/process_data/convex_hull.py --i $INPUT --manifold-path /tmp/Manifold/build/ --faces 5000
-# fi
 
-python main.py --input-pc $INPUT \
+if [ ! -e $INPUT2 ]; then
+    echo "$INPUT2 not found" && \
+    python scripts/process_data/poisson_disk.py $INPUT
+    else
+        echo "$INPUT2 found"
+fi
+
+if [ ! -e $HULL ]; then
+    echo "$HULL not found" && \
+    python scripts/process_data/convex_hull.py --i $INPUT2 --manifold-path /tmp/Manifold/build/ --faces 5000
+    else
+        echo "$HULL found"
+fi
+
+
+python main.py --input-pc $INPUT2 \
 --initial-mesh $HULL \
 --save-path $SAVEDIR \
---iterations 1000 \
---export-interval 200
+--iterations 5000 \
+--export-interval 500 \
+--max-faces 20000 \
+# --begin-samples 3000 \
+--upsamp 100 \
+# --beamgap-iterations 300
